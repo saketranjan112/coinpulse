aa5430c112462007b752299d5d97f5df56f1221e
Saket <saketranjan112@gmail.com>
2025-12-31 22:56:25 +0530

    implemented navigation

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
+const Header = () => {
+    const pathname = usePathname();
+
+    return (
+        <header>
+            <div className="main-container inner">
+                <Link href="/">
+                    <Image src="logo.svg" width={132} height={40} alt='logo' />
+                </Link>
+
+                <nav>
+                    <Link href="/" className={cn('nav-link', {'is-active': pathname === '/', 'is-home': true})}>Home</Link>
+
+                    <p>Search modal</p>
+
+                    <Link href="/coins" className={cn('nav-link', {'is-active': pathname === '/coins'})}>All coins</Link>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
