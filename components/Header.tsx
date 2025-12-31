a7c2ef02ae06dc4fdb56b7eab3dc2cea353e12d8
Saket <saketranjan112@gmail.com>
2025-12-31 22:59:58 +0530

    test change

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
                 <nav>
                     <Link href="/" className={cn('nav-link', {'is-active': pathname === '/', 'is-home': true})}>Home</Link>
 
-                    <p>Search modal</p>
+                    <p>Search modal here</p>
 
                     <Link href="/coins" className={cn('nav-link', {'is-active': pathname === '/coins'})}>All coins</Link>
                 </nav>
